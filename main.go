371f42025ad0cfc267b2619f0e2b47bab95664e4
Stephan Ziegler <stephan.ziegler@sixt.com>
2017-05-19 11:16:00 +0200

    finished json response, image and solution are equal

--- main.go
+++ main.go
@@ -6,24 +6,44 @@ import (
 	"log"
 	"net/http"
 	"encoding/json"
+	"encoding/base64"
+	"bytes"
+	"image/jpeg"
+	"strconv"
+	"strings"
 )
 
 type CaptchaResponse struct {
-	ImageBase64 captcha.Image `json:"image"`
-	Solution []byte `json:"solution"`
+	Image string `json:"image"`
+	Solution string `json:"solution"`
 }
 
 func getCaptcha(w http.ResponseWriter, r *http.Request) {
 	myCaptcha := captcha.New()
 
-	foo := captcha.RandomDigits(6)
-	image := captcha.NewImage(myCaptcha, foo, captcha.StdWidth, captcha.StdHeight)
-	captchaResponse := CaptchaResponse{*image, foo}
+	digits := captcha.RandomDigits(6)
+	image := captcha.NewImage(myCaptcha, digits, captcha.StdWidth, captcha.StdHeight)
+
+	solution := convert(digits)
+
+	buffer := new(bytes.Buffer)
+	jpeg.Encode(buffer, image, nil)
+	img := base64.StdEncoding.EncodeToString(buffer.Bytes())
+
+	captchaResponse := CaptchaResponse{img, solution}
 
 	b, _ := json.Marshal(captchaResponse)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(b)
+}
+
+func convert( b []byte ) string {
+	s := make([]string,len(b))
+	for i := range b {
+		s[i] = strconv.Itoa(int(b[i]))
+	}
+	return strings.Join(s,"")
 }
 
 func main() {
