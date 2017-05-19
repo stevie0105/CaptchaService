96fe2880616bb716568520edab1c04f8f7593a0f
Stephan Ziegler <stephan.ziegler@sixt.com>
2017-05-19 13:05:00 +0200

    errorhandling + refactoring

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"bytes"
 	"image/jpeg"
 	"strconv"
-	"strings"
 )
 
 type CaptchaResponse struct {
@@ -18,7 +17,7 @@ type CaptchaResponse struct {
 	Solution string `json:"solution"`
 }
 
-func getCaptcha(w http.ResponseWriter, r *http.Request) {
+func getCaptcha() ([]byte, error) {
 	myCaptcha := captcha.New()
 
 	digits := captcha.RandomDigits(6)
@@ -32,22 +31,39 @@ func getCaptcha(w http.ResponseWriter, r *http.Request) {
 
 	captchaResponse := CaptchaResponse{img, solution}
 
-	b, _ := json.Marshal(captchaResponse)
+	b, err := json.Marshal(captchaResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func handleCaptcha(w http.ResponseWriter, r *http.Request) {
+	b, err := getCaptcha()
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(b)
 }
 
-func convert( b []byte ) string {
-	s := make([]string,len(b))
+func convert(b []byte) string {
+	var buffer bytes.Buffer
 	for i := range b {
-		s[i] = strconv.Itoa(int(b[i]))
+		buffer.WriteString(strconv.Itoa(int(b[i])))
 	}
-	return strings.Join(s,"")
+	return buffer.String()
+}
+
+func init() {
+	http.HandleFunc("/v2/getCaptcha", handleCaptcha)
 }
 
 func main() {
-	http.HandleFunc("/", getCaptcha)
 	fmt.Println("Server is at localhost:8666")
 	if err := http.ListenAndServe("localhost:8666", nil); err != nil {
 		log.Fatal(err)
